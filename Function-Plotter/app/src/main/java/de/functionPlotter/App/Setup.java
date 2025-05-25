89405da9408693eac9655916bbadbf08610831de
Ole Bockhorst <nils.ole.bockhorst@mni.thm.de>
2025-05-25 22:33:56 +0200

    Extension: Added X as default variable for ast plotting

--- Function-Plotter/app/src/main/java/de/functionPlotter/App/Setup.java
+++ Function-Plotter/app/src/main/java/de/functionPlotter/App/Setup.java
@@ -7,10 +7,15 @@ public class Setup {
 
     public static void setUp() {
         setUpConstants();
+        setUpVariables();
     }
 
     private static void setUpConstants() {
         GlobalContext.VARIABLES.add("pi", new ValueNode(Math.PI));
         GlobalContext.VARIABLES.add("e", new ValueNode(Math.E));
+    }
+
+    private static void setUpVariables() {
+        GlobalContext.VARIABLES.add("x", new ValueNode(0)); // Default value for x
     }
 }
