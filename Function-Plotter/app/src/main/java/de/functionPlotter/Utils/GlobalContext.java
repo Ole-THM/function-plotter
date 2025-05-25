c3bc5e08eca532c54e90fe924a5e1ea580022784
Ole Bockhorst <nils.ole.bockhorst@mni.thm.de>
2025-05-25 22:36:11 +0200

    Extension: Added new Global Variables for plotting purposes

--- Function-Plotter/app/src/main/java/de/functionPlotter/Utils/GlobalContext.java
+++ Function-Plotter/app/src/main/java/de/functionPlotter/Utils/GlobalContext.java
@@ -1,10 +1,11 @@
 package de.functionPlotter.Utils;
 
-import de.functionPlotter.AbstractSyntaxTree.AST;
-import de.functionPlotter.ParsingEngine.Parser.Parser;
+import de.functionPlotter.Plot.XYRange;
 
 public class GlobalContext {
 
     public static final Variables VARIABLES = new Variables();
-
+    public static XYRange xyRange= new XYRange(-10,10,-10,10); // [xMin, xMax]
+    public static StringBuilder outputString = new StringBuilder();
+    public static int[] outputDimensions = new int[]{1280, 720}; // [width, height]
 }
