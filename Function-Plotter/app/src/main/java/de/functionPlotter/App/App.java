23233ee8f14b7f2e8d01c273496e2f5a424fdb7b
Ole Bockhorst <nils.ole.bockhorst@mni.thm.de>
2025-05-25 15:37:09 +0200

    Update: Updated debug statements

--- Function-Plotter/app/src/main/java/de/functionPlotter/App/App.java
+++ Function-Plotter/app/src/main/java/de/functionPlotter/App/App.java
@@ -1,19 +1,23 @@
 package de.functionPlotter.App;
 
 import de.functionPlotter.AbstractSyntaxTree.ASTNodeI;
-import de.functionPlotter.ParsingEngine.Parser.InfixParser;
+import de.functionPlotter.ParsingEngine.Parser.Parser;
 import de.functionPlotter.Utils.GlobalContext;
 
 import java.text.ParseException;
 
 public class App {
-    public static void main(String[] args) throws ParseException {
 
+    public static void main(String[] args) throws ParseException {
         System.out.println("Hello, World!");
-        InfixParser parser = new InfixParser();
-        GlobalContext.VARIABLES.add("x", parser.parse("2 + 3"));
-        ASTNodeI ast = parser.parse("10^2");
+        Setup.setUp();
+        GlobalContext.VARIABLES.add("x", Parser.parseInfix("2 + 3"));
+        ASTNodeI ast = Parser.parseInfix("sin(10)");
         System.out.println(ast.toStringInfix());
         System.out.println(ast.evaluate());
+        ASTNodeI ast2 = Parser.parseRPN("10 sin");
+        System.out.println(ast2.evaluate());
     }
+
 }
+
