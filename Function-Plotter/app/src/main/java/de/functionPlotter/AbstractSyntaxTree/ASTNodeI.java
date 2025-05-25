22093db216f616d469ffca0f1b8b73beae3c2d67
Ole Bockhorst <nils.ole.bockhorst@mni.thm.de>
2025-05-25 15:48:17 +0200

    Updated: Switched to use sealed interface

--- Function-Plotter/app/src/main/java/de/functionPlotter/AbstractSyntaxTree/ASTNodeI.java
+++ Function-Plotter/app/src/main/java/de/functionPlotter/AbstractSyntaxTree/ASTNodeI.java
@@ -1,6 +1,6 @@
 package de.functionPlotter.AbstractSyntaxTree;
 
-public interface ASTNodeI {
+public sealed interface ASTNodeI permits AST, BinaryOpNode, FunctionCallNode, UnaryOpNode, ValueNode, VariableNode {
     double evaluate();
     String toStringInfix();
 }
