aaf7a92da509c4468c4fda226e201a8cc82dec75
Ole Bockhorst <nils.ole.bockhorst@mni.thm.de>
2025-05-25 15:33:51 +0200

    Extension: Added Unary Minus TokenType

--- Function-Plotter/app/src/main/java/de/functionPlotter/ParsingEngine/Lexer/TokenType.java
+++ Function-Plotter/app/src/main/java/de/functionPlotter/ParsingEngine/Lexer/TokenType.java
@@ -3,7 +3,7 @@ package de.functionPlotter.ParsingEngine.Lexer;
 public enum TokenType {
     NUMBER,                                             // Numeric literals (integers, floats)
     IDENTIFIER,                                         // Identifiers (variable names)
-    FUNCTION,                                           // Native functions like sin, cos, log, sqrt etc.
+    FUNCTION, UNARYMINUS,                               // Native functions like sin, cos, log, sqrt etc.
     PLUS, MINUS, MULTIPLY, DIVIDE, EXPONENT,            // Arithmetic operators
     OPENPARENTHESIS, CLOSEPARENTHESIS,                  // Parentheses
     EOF                                                 // End of file token
