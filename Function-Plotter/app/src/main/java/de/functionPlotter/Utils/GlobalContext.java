07b1f196e7cad8a6971cfdabd68b33250e894a36
Ole Bockhorst <nils.ole.bockhorst@mni.thm.de>
2025-05-25 13:48:32 +0200

    Extension: Added GlobalContext File for storing sessions variables

---
+++ Function-Plotter/app/src/main/java/de/functionPlotter/Utils/GlobalContext.java
@@ -0,0 +1,10 @@
+package de.functionPlotter.Utils;
+
+import de.functionPlotter.AbstractSyntaxTree.AST;
+import de.functionPlotter.ParsingEngine.Parser.Parser;
+
+public class GlobalContext {
+
+    public static final Variables VARIABLES = new Variables();
+
+}
