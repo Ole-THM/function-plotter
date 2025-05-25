da747ca887385740eb396115660f78934c322234
Ole Bockhorst <nils.ole.bockhorst@mni.thm.de>
2025-05-25 22:35:04 +0200

    Extension: Added general Parse-Method

--- Function-Plotter/app/src/main/java/de/functionPlotter/ParsingEngine/Parser/Parser.java
+++ Function-Plotter/app/src/main/java/de/functionPlotter/ParsingEngine/Parser/Parser.java
@@ -9,6 +9,16 @@ public class Parser {
     private static final InfixParser infixParser = new InfixParser();
     private static final RPNParser RPNParser = new RPNParser();
 
+    public static ASTNodeI parse(String expression) throws ParseException {
+        if (infixParser.isValid(expression)) {
+            return infixParser.parse(expression);
+        } else if (RPNParser.isValid(expression)) {
+            return RPNParser.parse(expression);
+        } else {
+            throw new ParseException("Invalid expression: " + expression, 0);
+        }
+    }
+
     public static ASTNodeI parseInfix(String expression) throws ParseException {
         return infixParser.parse(expression);
     }
