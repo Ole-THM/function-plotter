b908d27203d0388072a07a0916250fc7070c1243
Ole Bockhorst <nils.ole.bockhorst@mni.thm.de>
2025-05-26 21:54:27 +0200

    Update: changed test cases for the rpnparser to use toStringRPN method now

--- Function-Plotter/app/src/test/java/de/functionPlotter/ParsingEngine/Parser/RPNParserTest.java
+++ Function-Plotter/app/src/test/java/de/functionPlotter/ParsingEngine/Parser/RPNParserTest.java
@@ -21,58 +21,56 @@ class RPNParserTest {
     void testSimpleAddition() throws ParseException {
         ASTNodeI ast = parser.parse("2 3 +");
         assertEquals(5.0, ast.evaluate(), 1e-9);
-        assertEquals("2 + 3", ast.toStringInfix());
+        assertEquals("2 3 +", ast.toStringRPN());
     }
 
     @Test
     void testSimpleSubtraction() throws ParseException {
         ASTNodeI ast = parser.parse("5 2 -");
         assertEquals(3.0, ast.evaluate(), 1e-9);
-        assertEquals("5 - 2", ast.toStringInfix());
+        assertEquals("5 2 -", ast.toStringRPN());
     }
 
     @Test
     void testSimpleMultiplication() throws ParseException {
         ASTNodeI ast = parser.parse("4 3 *");
         assertEquals(12.0, ast.evaluate(), 1e-9);
-        assertEquals("4 * 3", ast.toStringInfix());
+        assertEquals("4 3 *", ast.toStringRPN());
     }
 
     @Test
     void testSimpleDivision() throws ParseException {
         ASTNodeI ast = parser.parse("8 2 /");
         assertEquals(4.0, ast.evaluate(), 1e-9);
-        assertEquals("8 / 2", ast.toStringInfix());
+        assertEquals("8 2 /", ast.toStringRPN());
     }
 
     @Test
     void testOperatorPrecedence() throws ParseException {
         ASTNodeI ast = parser.parse("2 3 * 4 +");
         assertEquals(10.0, ast.evaluate(), 1e-9);
-        assertEquals("2 * 3 + 4", ast.toStringInfix());
+        assertEquals("2 3 * 4 +", ast.toStringRPN());
     }
 
     @Test
     void testExponentiation() throws ParseException {
         ASTNodeI ast = parser.parse("2 3 ^");
         assertEquals(8.0, ast.evaluate(), 1e-9);
-        assertEquals("2 ^ 3", ast.toStringInfix());
+        assertEquals("2 3 ^", ast.toStringRPN());
     }
 
     @Test
     void testFunctionCall() throws ParseException {
-        // Assuming you have a way to define variables or functions in RPN
-        // This test might need adjustment based on your UPN syntax for functions
         ASTNodeI ast = parser.parse("0 sin");
         assertEquals(0.0, ast.evaluate(), 1e-9);
-        assertEquals("sin(0)", ast.toStringInfix());
+        assertEquals("0 sin", ast.toStringRPN());
     }
 
     @Test
     void testMoreComplexExpression() throws ParseException {
         ASTNodeI ast = parser.parse("3 4.0001 + 2 * 7 /");
         assertEquals((3 + 4.0001) * 2 / 7, ast.evaluate(), 1e-9);
-        assertEquals("(3 + 4,0001) * 2 / 7", ast.toStringInfix());
+        assertEquals("3 4,0001 + 2 * 7 /", ast.toStringRPN());
     }
 
 }
