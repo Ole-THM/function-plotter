67a1fa8be1f17ae12a41c9dd1edce8d464e313b4
Ole Bockhorst <nils.ole.bockhorst@mni.thm.de>
2025-05-26 21:31:48 +0200

    Update: added eval and toString support for the log function to handle one or two arguments

--- Function-Plotter/app/src/main/java/de/functionPlotter/AbstractSyntaxTree/FunctionCallNode.java
+++ Function-Plotter/app/src/main/java/de/functionPlotter/AbstractSyntaxTree/FunctionCallNode.java
@@ -10,9 +10,15 @@ public record FunctionCallNode(String functionName, List<ASTNodeI> arguments) im
             case "cos" -> Math.cos(this.arguments.getFirst().evaluate());
             case "tan" -> Math.tan(this.arguments.getFirst().evaluate());
             case "sqrt" -> Math.sqrt(this.arguments.getFirst().evaluate());
-            case "log" -> Math.log(this.arguments.getFirst().evaluate());
+            case "log" -> this.arguments.get(1) != null
+                              ? logn(this.arguments.getFirst().evaluate(), this.arguments.get(1).evaluate())
+                              : logn(Math.E, this.arguments.get(0).evaluate()); // defaults to natural log if no base is given
             default -> throw new UnsupportedOperationException("Unsupported function: " + functionName);
         };
+    }
+
+    private double logn(double base, double value) {
+        return Math.log(value) / Math.log(base);
     }
 
     @Override
@@ -22,7 +28,9 @@ public record FunctionCallNode(String functionName, List<ASTNodeI> arguments) im
             case "cos" -> "cos(" + this.arguments.getFirst().toStringInfix() + ")";
             case "tan" -> "tan(" + this.arguments.getFirst().toStringInfix() + ")";
             case "sqrt" -> "sqrt(" + this.arguments.getFirst().toStringInfix() + ")";
-            case "log" -> "log(" + this.arguments.getFirst().toStringInfix() + ")";
+            case "log" -> "log(" + (this.arguments.get(1) != null
+                    ? this.arguments.getFirst().toStringInfix() + ", " + this.arguments.get(1).toStringInfix()
+                    : String.format("%.4f", Math.E) + ", " + this.arguments.get(0).toStringInfix()) + ")";
             default -> throw new UnsupportedOperationException("Unsupported function: " + functionName);
         };
 
