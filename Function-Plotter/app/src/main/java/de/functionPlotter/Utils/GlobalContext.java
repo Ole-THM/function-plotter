7695bd18452ccafe94a25e308963b4332ed07eae
Ole Bockhorst <nils.ole.bockhorst@mni.thm.de>
2025-05-27 13:08:08 +0200

    Update: updated GlobalContext according to other changes

--- Function-Plotter/app/src/main/java/de/functionPlotter/Utils/GlobalContext.java
+++ Function-Plotter/app/src/main/java/de/functionPlotter/Utils/GlobalContext.java
@@ -1,11 +1,12 @@
 package de.functionPlotter.Utils;
 
-import de.functionPlotter.Plot.XYRange;
+import de.functionPlotter.Plot.PlotUtils.XYRange;
+import de.functionPlotter.Plot.PlotUtils.outputDimension;
 
 public class GlobalContext {
 
     public static final Variables VARIABLES = new Variables();
-    public static XYRange xyRange= new XYRange(-10,10,-10,10); // [xMin, xMax]
+    public static XYRange xyRange= new XYRange(-10,10,-5,5); // [xMin, xMax]
     public static StringBuilder outputString = new StringBuilder();
-    public static int[] outputDimensions = new int[]{1280, 720}; // [width, height]
+    public static outputDimension outputDimensions = new outputDimension(1000, 700); // [width, height]
 }
